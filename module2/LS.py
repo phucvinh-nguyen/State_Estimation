d1d29dd0c0d2f3ab30b43607ffc2b57c4ff982bd
phucvinh <phucvinhng01@gmail.com>
2024-07-21 18:40:22 +0700

    begin:first commit

---
+++ module2/LS.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+# Store the voltage and current data as column vectors.
+I = np.array([[0.2, 0.3, 0.4, 0.5, 0.6]]).T
+V = np.array([[1.23, 1.38, 2.06, 2.47, 3.17]]).T
+
+plt.scatter(I, V)
+plt.xlabel('Current (A)')
+plt.ylabel('Voltage (V)')
+plt.grid(True)
+plt.show()
+
+# Define the H matrix - what does it contain?
+H = np.array([[1, 1, 1, 1, 1]]).T
+
+H_T = H.T #Transpose of H
+
+# Define the y matrix - what does it contain?
+y = V / I
+print (y)
+# Now estimate the resistance parameter.
+R = np.linalg.inv(H_T @ H) @ H_T @ y
+
+
+print('The slope parameter of the best-fit line (i.e., the resistance) is:')
+print(R[0, 0])
+
+I_line = np.arange(0, 0.8, 0.1).reshape(8, 1)
+print (I_line)
+V_line = R*I_line
+
+plt.scatter(I, V)
+plt.plot(I_line, V_line)
+plt.xlabel('Current (A)')
+plt.ylabel('Voltage (V)')
+plt.grid(True)
+plt.show()
